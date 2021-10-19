d5dcedf8f2afb2384af87be73cd74ccc5af60d8a
JavaKira <JavaKira@yandex.ru>
2021-10-19 22:37:56 +0300

    Added Tile

---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    [SerializeField] private Sprite sprite;
+
+    private Vector2 _inTilemapPosition = new Vector2();
+    private void Start()
+    {
+        GetComponent<SpriteRenderer>().sprite = sprite;
+    }
+}
