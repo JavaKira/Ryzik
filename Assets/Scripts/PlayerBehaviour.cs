5668603ca4a9ffd6e5e61d75a6eb57d9b645b6a3
JavaKira <JavaKira@yandex.ru>
2021-10-19 23:24:52 +0300

    Added PlayerBehaviour

---
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,26 @@
+using System;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    [SerializeField] private float speed;
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.W)) Move(Vector2.up);
+        
+        if (Input.GetKey(KeyCode.S)) Move(Vector2.down);
+        
+        if (Input.GetKey(KeyCode.A)) Move(Vector2.left);
+        
+        if (Input.GetKey(KeyCode.D)) Move(Vector2.right);
+    }
+
+    public void Move(Vector2 direction)
+    {
+        var moveDirectionForce = new Vector3(direction.x, direction.y , 0.0f);
+        moveDirectionForce *= speed;
+        
+        transform.Translate(moveDirectionForce * Time.fixedTime);
+    }
+}
