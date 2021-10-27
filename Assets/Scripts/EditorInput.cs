384d0859ddfa79bb23082a42ec02589c779c5cef
JavaKira <JavaKira@yandex.ru>
2021-10-27 14:27:08 +0300

    Added EditorInput

---
+++ Assets/Scripts/EditorInput.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+public class EditorInput : MonoBehaviour
+{
+    [SerializeField] private GameObject cameraPoint;
+    [SerializeField] private float moveSpeed;
+    private void FixedUpdate()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.W)) Move(Vector2.up);
+        
+        if (Input.GetKey(KeyCode.S)) Move(Vector2.down);
+        
+        if (Input.GetKey(KeyCode.A)) Move(Vector2.left);
+        
+        if (Input.GetKey(KeyCode.D)) Move(Vector2.right);
+    }
+
+    private void Move(Vector3 direction)
+    {
+        cameraPoint.transform.position += direction * (moveSpeed * Time.fixedDeltaTime);
+    }
+}
