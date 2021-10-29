bc9619d13e2e5201cda85b2e403928b9f08e27ba
JavaKira <JavaKira@yandex.ru>
2021-10-29 16:24:51 +0300

    Added Size property and width height field

--- Assets/Scripts/TileMap.cs
+++ Assets/Scripts/TileMap.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Drawing;
 using UnityEngine;
 
 public class TileMap : MonoBehaviour
@@ -7,6 +8,10 @@ public class TileMap : MonoBehaviour
     [SerializeField] private TileBehaviour tilePrefab;
 
     private Tile[][] _tiles;
+    private int _width, _height;
+
+    public Vector2 Size => new Vector2(_width, _height);
+
     private void Start()
     {
         DebugWorld();
@@ -16,34 +21,36 @@ public class TileMap : MonoBehaviour
     // after i made load from file
     private void DebugWorld()
     {
-        _tiles = new Tile[100][];
+        _width = 100;
+        _height = 100;
+        _tiles = new Tile[_width][];
 
-        for (var i = 0; i < _tiles.Length; i++)
+        for (var x = 0; x < _width; x++)
         {
-            _tiles[i] = new Tile[100];
+            _tiles[x] = new Tile[_height];
             
-            for (var x = 0; x < _tiles[i].Length;x++)
+            for (var y = 0; y < _height; y++)
             {
                 var tile = new Tile
                 {
                     Floor = Floor.GetByName("Grass"),
-                    InTilemapPosition = new Vector3(i * tilesGap, x * tilesGap)
+                    InTilemapPosition = new Vector3(x * tilesGap, y * tilesGap)
                 };
-                _tiles[i][x] = tile;
+                _tiles[x][y] = tile;
             }
         }
 
-        for (int x = 0; x < 10; x++)
+        for (var x = 0; x < 10; x++)
         {
-            for (int y = 0; y < 10; y++)
+            for (var y = 0; y < 10; y++)
             {
                 _tiles[x + 30][y + 30].Block = Block.GetByName("WoodenPlank");
             }
         }
         
-        for (int x = 0; x < 8; x++)
+        for (var x = 0; x < 8; x++)
         {
-            for (int y = 0; y < 8; y++)
+            for (var y = 0; y < 8; y++)
             {
                 _tiles[x + 31][y + 31].Block = Block.GetAir();
                 _tiles[x + 31][y + 31].Floor = Floor.GetByName("WoodenPlank");
