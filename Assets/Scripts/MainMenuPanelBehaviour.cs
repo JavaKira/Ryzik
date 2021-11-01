1f2f4b39199e9780f41476fe10bdee6fac55044d
JavaKira <JavaKira@yandex.ru>
2021-11-01 22:53:38 +0300

    Added simple MainMenu ui

---
+++ Assets/Scripts/MainMenuPanelBehaviour.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuPanelBehaviour : MonoBehaviour
+{
+    [SerializeField] private GameObject middlePanel;
+    
+    public void PlayButton()
+    {
+        middlePanel.SetActive(true);
+    }
+}
