a41ec10a873fbe13293be857f774559d7952693e
JavaKira <JavaKira@yandex.ru>
2021-11-01 21:06:42 +0300

    Added DefaultEditorMap and loading it when starting the game

--- Assets/Scripts/EditorMapLoader.cs
+++ Assets/Scripts/EditorMapLoader.cs
@@ -1,12 +1,25 @@
-﻿using IO;
+﻿using System;
+using IO;
 using TMPro;
 using UnityEngine;
 
 public class EditorMapLoader : MonoBehaviour
 {
+    private const string DefaultMapName = "DefaultEditorMap";
+    
     [SerializeField] private TMP_InputField mapNameField;
     [SerializeField] private Map map;
 
+    private void Start()
+    {
+        LoadDefault();
+    }
+
+    private void LoadDefault()
+    {
+        MapIO.Load(map, DefaultMapName);
+    }
+    
     public void Load()
     {
         if (mapNameField.text.Length != 0)
