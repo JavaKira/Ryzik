391b33a460f7cc25a874ff1713f08b24c7830709
JavaKira <JavaKira@yandex.ru>
2021-11-02 11:04:16 +0300

    Added MapSelectBuilder

---
+++ Assets/Scripts/MapSelectBuilder.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+
+public class MapSelectBuilder : MonoBehaviour
+{
+    [SerializeField] private string[] maps;
+    [SerializeField] private float gap;
+    [SerializeField] private MapButton mapButtonPrefab;
+
+    private void Start()
+    {
+        Build();
+    }
+
+    private void Build()
+    {
+        for (var i = 0; i < maps.Length; i++)
+        {
+            var mapButton = Instantiate(
+                mapButtonPrefab,
+                transform
+            );
+
+            var transform1 = mapButton.transform;
+            var mapButtonRectangleTransform = (RectTransform) transform1;
+            
+            transform1.localPosition += new Vector3(
+                0,
+                i * mapButtonRectangleTransform.sizeDelta.y + gap * i,
+                0
+            );
+            
+            mapButton.Build(maps[i]);
+        }
+    }
+}
