b3fb93e7852e83bc02868b6f01db50feadbd3f5f
JavaKira <JavaKira@yandex.ru>
2021-11-16 18:07:36 +0300

    Added MainMenu game start listener for buttons

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -1,11 +1,27 @@
 ﻿using System;
+using IO;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-/*init class*/
 public class Game : MonoBehaviour
 {
+    private static string _lastMapName;
+
     private void Awake()
     {
         Content.Init();
+        
+        if (_lastMapName != null)
+        {
+            MapIO.Load(FindObjectOfType<Map>(), _lastMapName);
+        }
+        
+        _lastMapName = string.Empty;
+    }
+    
+    public static void Open(string name)
+    {
+        _lastMapName = name;
+        SceneManager.LoadScene("SampleScene");
     }
 }
--- Assets/Scripts/MapButton.cs
+++ Assets/Scripts/MapButton.cs
@@ -1,10 +1,26 @@
-﻿using TMPro;
+﻿using System;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MapButton : MonoBehaviour
 {
+    private string _mapName;
+
+    private void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(OpenMap);
+    }
+
+    private void OpenMap()
+    {
+        if (_mapName != null)
+            Game.Open(_mapName);
+    }
+    
     public void Build(string mapName)
     {
+        _mapName = mapName;
         GetComponentInChildren<TMP_Text>().text = mapName;
     }
 }
