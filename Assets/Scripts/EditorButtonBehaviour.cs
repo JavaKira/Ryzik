d8d7305aee14311fb29b8921e624d8ca2b9cdccd
JavaKira <JavaKira@yandex.ru>
2021-11-17 15:02:12 +0300

    Added editor button in main menu

---
+++ Assets/Scripts/EditorButtonBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class EditorButtonBehaviour : MonoBehaviour
+{
+    private void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(OpenEditor);
+    }
+
+    private static void OpenEditor()
+    {
+        SceneManager.LoadScene("EditorScene");
+    }
+}
