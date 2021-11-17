e3edc715240c5ce25ad64af641fabcb721bbdb60
JavaKira <JavaKira@yandex.ru>
2021-11-17 15:18:28 +0300

    Added Joystick in editor

---
+++ Assets/Scripts/EditorCameraJoystickInspector.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+public class EditorCameraJoystickInspector : MonoBehaviour
+{
+     [SerializeField] private GameObject cameraPoint;
+     [SerializeField] private float speedMultiplier;
+
+     private Joystick _joystick;
+
+     private void Awake()
+     {
+         _joystick = GetComponent<Joystick>();
+     }
+
+     private void FixedUpdate()
+     {
+         UpdateCameraPoint();
+     }
+
+     private void UpdateCameraPoint()
+     {
+         var pos = _joystick.GetNormalized() * (speedMultiplier * Time.deltaTime);
+         cameraPoint.transform.position += new Vector3(pos.x, pos.y, 0);
+     }
+}
