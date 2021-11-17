220f6d8413908330ea4ad0be630ce08917fc7385
JavaKira <JavaKira@yandex.ru>
2021-11-17 07:06:42 +0300

    Added PlayerJoystickInspector

---
+++ Assets/Scripts/PlayerJoystickInspector.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class PlayerJoystickInspector : MonoBehaviour
+{
+    [SerializeField] private PlayerBehaviour player;
+    
+    private Joystick _joystick;
+    private Vector2 _oldJoystickPosition;
+
+    private void Start()
+    {
+        _joystick = GetComponent<Joystick>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (player != null)
+            UpdatePlayer();
+        
+        _oldJoystickPosition = _joystick.GetNormalized();
+    }
+
+    private void UpdatePlayer()
+    {
+        player.Move(new Vector2(_joystick.GetNormalized().x, _joystick.GetNormalized().y));
+    }
+}
