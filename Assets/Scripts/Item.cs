bb83592d72d02bd283078424ae8f8983a81b8a6e
JavaKira <JavaKira@yandex.ru>
2021-11-18 22:22:51 +0300

    Added Item

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    [SerializeField] private Sprite sprite;
+
+    public static Item GetByName(string name)
+    {
+        return Resources.Load<Item>("items/" + name);
+    }
+    
+    public static Item[] GetAll()
+    {
+        return Resources.LoadAll<Item>("items/");
+    }
+}
