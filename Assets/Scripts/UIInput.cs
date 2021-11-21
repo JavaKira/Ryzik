6f7625f00e1a532a98e60b9ce0d08653fefb2add
JavaKira <JavaKira@yandex.ru>
2021-11-21 09:35:20 +0300

    Added Inventory layout and UIInput

---
+++ Assets/Scripts/UIInput.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+
+public class UIInput : MonoBehaviour
+{
+    [SerializeField] private Joystick joystick;
+    [SerializeField] private GameObject inventory;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (inventory.activeSelf)
+                CloseInventory();
+            else
+                OpenInventory();
+        }
+    }
+
+    public void OpenInventory()
+    {
+        inventory.SetActive(true);
+        joystick.gameObject.SetActive(false);
+    }
+
+    public void CloseInventory()
+    {
+        inventory.SetActive(false);
+        joystick.gameObject.SetActive(true);
+    }
+}
