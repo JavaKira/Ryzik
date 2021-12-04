f84b9405a90810355e816f06972569e962b0c22b
JavaKira <JavaKira@yandex.ru>
2021-12-04 14:58:57 +0300

    Added Campaign Camera Input

---
+++ Assets/Scripts/CampaignCameraInput.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+public class CampaignCameraInput : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.touchCount == 0) return;
+
+        var touch = Input.GetTouch(Input.touchCount - 1);
+        transform.position -= new Vector3(touch.deltaPosition.x * touch.deltaTime, touch.deltaPosition.y * touch.deltaTime);
+    }
+}
