909ee309923fb0b0011e4abbcb08e58037d7b411
JavaKira <JavaKira@yandex.ru>
2021-12-04 06:45:49 +0300

    Added CampaignPoint

---
+++ Assets/Scripts/UI/CampaignPoint.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class CampaignPoint : MonoBehaviour
+    {
+        [SerializeField] private Color noCompletedColor;
+        [SerializeField] private Color completedColor;
+
+        private bool _completed = default;
+        private string _title;
+
+        private void Start()
+        {
+            _title = GetComponentInChildren<TMP_Text>().text;
+            
+            GetComponent<Image>().color = _completed ? 
+                new Color(completedColor.r, completedColor.g, completedColor.b) : 
+                new Color(noCompletedColor.r, noCompletedColor.g, noCompletedColor.b);
+            
+            GetComponent<Button>().onClick.AddListener(Open);
+        }
+
+        private void Open()
+        {
+            Game.Open(_title, true);
+        }
+    }
+}
