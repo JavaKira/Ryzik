359924a5f98cf7446fab9e4586d3df5a1105e55d
JavaKira <JavaKira@yandex.ru>
2021-12-07 06:28:47 +0300

    Move PlayerBehaviour block place/break code to PlayerTilemapEditor

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -6,51 +6,11 @@ using UnityEngine;
 public class PlayerBehaviour : MonoBehaviour
 {
     [SerializeField] private float speed;
-    [SerializeField] private TileMap tileMap;
-    [SerializeField] private Hotbar hotbar;
-
-    private float _mouseDragTime;
+    
     private Vector3 _oldPosition;
 
     public Vector2 Speed => _oldPosition - transform.position;
 
-    private void Update()
-    {
-        if (Game.Instance.IsPause() || Input.touchCount == 0) return;
-        
-        var touch = Input.GetTouch(Input.touchCount - 1);
-        if (EditorInput.IsMouseOverUI(touch.fingerId)) return;
-        
-        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(touch.position);
-
-        if (touch.phase == TouchPhase.Stationary)
-            _mouseDragTime += Time.deltaTime;
-        else
-            _mouseDragTime = 0;
-        
-        if (_mouseDragTime > 0.5f)
-        {
-            var tile = tileMap.GetTile(
-                (int) ((mousePosition.x + 4) / 8),
-                (int) ((mousePosition.y + 4) / 8)
-            );
-            
-            if (tile.Block.name != "Air")
-                tile.Block = Block.GetAir();
-            else if (tile.Floor.name != "Air")
-                tile.Floor = Floor.GetAir();
-            
-            return;
-        }
-        
-        if (touch.phase == TouchPhase.Began && hotbar.SelectedSlot != null)
-        {
-            tileMap.GetTile(
-                    (int) ((mousePosition.x + 4) / 8), 
-                    (int) ((mousePosition.y + 4) / 8)
-            ).SetIContent(hotbar.SelectedSlot.Item);
-        }
-    }
     public void Move(Vector2 direction)
     {
         _oldPosition = transform.position;
---
+++ Assets/Scripts/PlayerTilemapEditor.cs
@@ -0,0 +1,49 @@
+﻿using Content;
+using UI;
+using UnityEngine;
+
+public class PlayerTilemapEditor : MonoBehaviour
+{
+    [SerializeField] private TileMap tileMap;
+    [SerializeField] private Hotbar hotbar;
+
+    private float _mouseDragTime;
+    
+    private void Update()
+    {
+        if (Game.Instance.IsPause() || Input.touchCount == 0) return;
+        
+        var touch = Input.GetTouch(Input.touchCount - 1);
+        if (EditorInput.IsMouseOverUI(touch.fingerId)) return;
+        
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(touch.position);
+
+        if (touch.phase == TouchPhase.Stationary)
+            _mouseDragTime += Time.deltaTime;
+        else
+            _mouseDragTime = 0;
+        
+        if (_mouseDragTime > 0.5f)
+        {
+            var tile = tileMap.GetTile(
+                (int) ((mousePosition.x + 4) / 8),
+                (int) ((mousePosition.y + 4) / 8)
+            );
+            
+            if (tile.Block.name != "Air")
+                tile.Block = Block.GetAir();
+            else if (tile.Floor.name != "Air")
+                tile.Floor = Floor.GetAir();
+            
+            return;
+        }
+        
+        if (touch.phase == TouchPhase.Began && hotbar.SelectedSlot != null)
+        {
+            tileMap.GetTile(
+                (int) ((mousePosition.x + 4) / 8), 
+                (int) ((mousePosition.y + 4) / 8)
+            ).SetIContent(hotbar.SelectedSlot.Item);
+        }
+    }
+}
