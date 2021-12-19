fd878d2409db109dbcc2a4a0c99fd69135edf848
JavaKira <JavaKira@yandex.ru>
2021-12-19 16:43:46 +0300

    Added Panel

---
+++ Assets/Scripts/UI/Panel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+namespace UI
+{
+    public class Panel : MonoBehaviour
+    {
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+                gameObject.SetActive(false);
+        }
+    }
+}
