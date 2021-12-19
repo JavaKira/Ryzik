94b1eb6eadbbe820dce59d0ad9d4671b7b2f3bee
JavaKira <JavaKira@yandex.ru>
2021-12-19 10:22:37 +0300

    Added ObjectPurse allowChangeObject

--- Assets/Scripts/ObjectPurse.cs
+++ Assets/Scripts/ObjectPurse.cs
@@ -4,6 +4,7 @@ public class ObjectPurse : MonoBehaviour
 {
     [SerializeField] private GameObject pursuedObject;
     [SerializeField] private float softness;
+    [SerializeField] private bool allowChangeObject = true;
     
     private void FixedUpdate()
     {
@@ -27,6 +28,7 @@ public class ObjectPurse : MonoBehaviour
 
     public void SetPurseObject(GameObject purse)
     {
-        pursuedObject = purse;
+        if (allowChangeObject)
+            pursuedObject = purse;
     }
 }
