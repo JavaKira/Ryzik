f3277bab9cf058be9ff7c3eed42a998d0867c0c9
JavaKira <JavaKira@yandex.ru>
2021-12-23 21:52:38 +0300

    Added EditorScene and SampleScene statusText when save map

--- Assets/Scripts/EditorMapSaver.cs
+++ Assets/Scripts/EditorMapSaver.cs
@@ -1,16 +1,22 @@
 ﻿using IO;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class EditorMapSaver : MonoBehaviour
 {
     [SerializeField] private TMP_InputField mapNameField;
     [SerializeField] private Map map;
 
+    public UnityEvent onSaveSuccessfully;
+    
     public void Save()
     {
         if (mapNameField.text.Length != 0)
+        {
+            onSaveSuccessfully.Invoke();
             MapIO.Save(map, mapNameField.text);
+        }
         else
             mapNameField.Select();
     }
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] private bool startAtStart;
+    [SerializeField] private float defaultTime;
+
+    public UnityEvent onEnd;
+    
+    private void Start()
+    {
+        if (startAtStart) 
+            StartTimer();
+    }
+
+    public void StartTimer()
+    {
+        StartCoroutine(StartEnumerator(defaultTime));
+    }
+
+    public void StartTimer(float time)
+    {
+        StartCoroutine(StartEnumerator(time));
+    }
+
+    private IEnumerator StartEnumerator(float time)
+    {
+        yield return new WaitForSeconds(time);
+        
+        onEnd.Invoke();
+    }
+}
