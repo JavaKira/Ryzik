a83153ff9f7f0074ec4df9bce6de95a095aa920e
JavaKira <JavaKira@yandex.ru>
2021-12-24 15:52:39 +0300

    Added SpriteVariants and added more sprite variants for floor grass

---
+++ Assets/Scripts/SpriteVariants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class SpriteVariants : MonoBehaviour
+{
+    [SerializeField] private Sprite[] variants;
+
+    private void Start()
+    {
+        if (variants.Length == 0) 
+            throw new ArgumentException("variants array could not be empty");
+        
+        GetComponent<SpriteRenderer>().sprite = variants[Random.Range(0, variants.Length)];
+    }
+}
