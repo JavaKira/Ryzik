531d5e7b787af219d8c1bb95665bc1499a8daeb7
JavaKira <JavaKira@yandex.ru>
2021-12-25 10:16:07 +0300

    Removed DefaultEditorMap, and make it generates at runtime

--- Assets/Scripts/EditorMapLoader.cs
+++ Assets/Scripts/EditorMapLoader.cs
@@ -1,23 +1,40 @@
 ﻿using System;
+using Content;
 using IO;
 using TMPro;
 using UnityEngine;
 
 public class EditorMapLoader : MonoBehaviour
 {
-    private const string DefaultMapName = "DefaultEditorMap";
-    
+    [SerializeField] private Vector2 defaultMapSize;
     [SerializeField] private TMP_InputField mapNameField;
     [SerializeField] private Map map;
 
     private void Start()
     {
-        LoadDefault();
+        GenerateMap();
     }
 
-    private void LoadDefault()
+    private void GenerateMap()
     {
-        MapIO.LoadFromResources(map, DefaultMapName);
+        map.Tilemap.Size = new Vector2Int((int) defaultMapSize.x, (int) defaultMapSize.y);
+        map.Tilemap.InitTilesArray();
+        
+        for (var x = 0; x < defaultMapSize.x; x++)
+        {
+            for (var y = 0; y < defaultMapSize.y; y++)
+            {
+                var tile = new Tile
+                {
+                    InTilemapPosition = new Vector2(x, y),
+                    Floor = Floor.GetByName("Grass")
+                };
+
+                map.Tilemap.SetTile(x, y, tile);
+            }
+        }
+        
+        map.Tilemap.Build();
     }
     
     public void Load()
