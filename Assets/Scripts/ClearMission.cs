c8c83a37d4a019ccbd974d2fc2128a5e1d313f5f
JavaKira <JavaKira@yandex.ru>
2021-12-25 20:17:48 +0300

    Added ClearMission

---
+++ Assets/Scripts/ClearMission.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UI;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ClearMission : MonoBehaviour
+{
+    private void Start()
+    {
+        Map.Instance.Mobs.Changed.AddListener(() =>
+        {
+            if (CheckEnd())
+                End();
+        });
+    }
+
+    private bool CheckEnd()
+    {
+        return Map.Instance.Mobs.GetByName("Cockroach").Count == 0;
+    }
+
+    private void End()
+    {
+        FindObjectOfType<MissionEndPanel>().enabled = true;
+    }
+}
---
+++ Assets/Scripts/UI/MissionEndPanel.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace UI
+{
+    public class MissionEndPanel : MonoBehaviour
+    {
+        public void Back()
+        {
+            SceneManager.LoadScene("CampaignRoadScene");
+        }
+    }
+}
