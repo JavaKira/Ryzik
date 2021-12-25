3aaa024f3216ec87f077ca4fbd6157ed1a03c935
JavaKira <JavaKira@yandex.ru>
2021-12-25 11:13:10 +0300

    Added Surrounded map, and fixed loading maps from resources

--- Assets/Scripts/UI/MapSelectBuilder.cs
+++ Assets/Scripts/UI/MapSelectBuilder.cs
@@ -18,34 +18,44 @@ namespace UI
         {
             var maps = new List<string>(MapIO.GetMapsList());
 
-            maps.Add("EmptyMap");
+            var resourcesMaps = new List<string>(new []{"EmptyMap", "Surrounded"});
 
             for (var i = 0; i < maps.Count; i++)
             {
-                var mapButton = Instantiate(
-                    mapButtonPrefab,
-                    transform
-                );
+                BuildItem(maps[i], false, i);
+            }
 
-                var transform1 = mapButton.transform;
-                var mapButtonRectangleTransform = (RectTransform) transform1;
-
-                transform1.localPosition += new Vector3(
-                    0,
-                    i * mapButtonRectangleTransform.sizeDelta.y + gap * i,
-                    0
-                );
-
-                mapButton.Build(maps[i]);
-
-                if (maps[i].Equals("EmptyMap"))
-                    mapButton.LoadFromResources();
+            for (var i = 0; i < resourcesMaps.Count; i++)
+            {
+                BuildItem(resourcesMaps[i], true, maps.Count + i);
             }
 
             ((RectTransform) transform).sizeDelta = new Vector2(
                 ((RectTransform) transform).sizeDelta.x,
                 (((RectTransform) mapButtonPrefab.transform).sizeDelta.y + gap) * maps.Count
             );
+        }
+
+        private void BuildItem(string mapName, bool loadFromResources, int number)
+        {
+            var mapButton = Instantiate(
+                mapButtonPrefab,
+                transform
+            );
+
+            var transform1 = mapButton.transform;
+            var mapButtonRectangleTransform = (RectTransform) transform1;
+
+            transform1.localPosition += new Vector3(
+                0,
+                number * mapButtonRectangleTransform.sizeDelta.y + gap * number,
+                0
+            );
+
+            mapButton.Build(mapName);
+
+            if (loadFromResources)
+                mapButton.LoadFromResources();
         }
     }
 }
