13503d96bfabb0429c1dcc94fb2b975edbf417ec
JavaKira <JavaKira@yandex.ru>
2021-12-27 10:43:07 +0300

    Added CampaignRoadScene back to main menu button

---
+++ Assets/Scripts/UI/SceneOpenButton.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class SceneOpenButton : MonoBehaviour
+    {
+        [SerializeField] private string sceneName;
+        
+        private void Awake()
+        {
+            GetComponent<Button>().onClick.AddListener(() =>
+            {
+                SceneManager.LoadScene(sceneName);
+            });
+        }
+    }
+}
