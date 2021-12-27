19602de6321882121bc8619a95a2f5b3f57b0816
JavaKira <JavaKira@yandex.ru>
2021-12-27 15:18:35 +0300

    Added PointConnectors and MissionStartPanel title set when open point

--- Assets/Scripts/UI/CampaignPoint.cs
+++ Assets/Scripts/UI/CampaignPoint.cs
@@ -7,19 +7,18 @@ namespace UI
 {
     public class CampaignPoint : MonoBehaviour
     {
+        [SerializeField] private string title;
         [SerializeField] private string missionType;
         [SerializeField] private Color noCompletedColor;
         [SerializeField] private Color completedColor;
 
         public string MissionType => missionType;
+        public string Title => title;
 
         private bool _completed = default;
-        private string _title;
 
         private void Start()
         {
-            _title = GetComponentInChildren<TMP_Text>().text;
-            
             GetComponent<Image>().color = _completed ? 
                 new Color(completedColor.r, completedColor.g, completedColor.b) : 
                 new Color(noCompletedColor.r, noCompletedColor.g, noCompletedColor.b);
--- Assets/Scripts/UI/MissionStartPanel.cs
+++ Assets/Scripts/UI/MissionStartPanel.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System;
+using TMPro;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 namespace UI
@@ -6,10 +8,11 @@ namespace UI
     public class MissionStartPanel : MonoBehaviour
     {
         private CampaignPoint _currentPoint;
-        
+
         public void Open(CampaignPoint campaignPoint)
         {
             _currentPoint = campaignPoint;
+            GetComponentInChildren<TMP_Text>().text = campaignPoint.Title;
             gameObject.SetActive(true);
         }
 
