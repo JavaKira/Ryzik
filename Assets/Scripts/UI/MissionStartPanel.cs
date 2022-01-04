9e32122755a13af8cbb489b277b18d4d7999058d
JavaKira <JavaKira@yandex.ru>
2022-01-04 14:53:47 +0300

    Added MissionStartPanel stars

--- Assets/Scripts/UI/MissionStartPanel.cs
+++ Assets/Scripts/UI/MissionStartPanel.cs
@@ -10,6 +10,12 @@ namespace UI
         public static CampaignPoint LastPoint;
         
         private CampaignPoint _currentPoint;
+        private Star[] _stars;
+
+        private void Awake()
+        {
+            _stars = GetComponentsInChildren<Star>();
+        }
 
         public void Open(CampaignPoint campaignPoint)
         {
@@ -20,12 +26,30 @@ namespace UI
             _currentPoint = campaignPoint;
             GetComponentInChildren<TMP_Text>().text = campaignPoint.Title;
             gameObject.SetActive(true);
+            
+            UpdateStars();
         }
 
         public void StartGame()
         {
             var missionType = _currentPoint.MissionType.Build();
             missionType.StartMission();
+        }
+
+        private void UpdateStars()
+        {
+            foreach (var star in _stars)
+            {
+                star.Active = false;
+            }
+
+            for (var i = 0; i < _currentPoint.Data.Stars; i++)
+            {
+                if (i >= _stars.Length)
+                    break;
+                
+                _stars[i].Active = true;
+            }
         }
     }
 }
