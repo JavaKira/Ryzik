e306e68d0ace27698a1c310b56a741142cdb72d2
JavaKira <JavaKira@yandex.ru>
2022-01-05 14:19:39 +0300

    Added CockroachAI

---
+++ Assets/Scripts/AI/CockroachAI.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Content;
+using UnityEngine;
+
+namespace AI
+{
+    public class CockroachAI : MonoBehaviour
+    {
+        [SerializeField] private Mob targetMobType;
+        
+        private Mob _target;
+        private bool _targetInBounds;
+
+        private void FixedUpdate()
+        {
+            if (!_targetInBounds) return;
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            var target = other.gameObject.GetComponent<Mob>();
+            if (target == null || !target.name.Equals(targetMobType.name)) return;
+            _targetInBounds = true;
+            _target = target;
+        }
+
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            var target = other.gameObject.GetComponent<Mob>();
+            if (target != null && target.Equals(_target))
+                _targetInBounds = false;
+        }
+    }
+}
