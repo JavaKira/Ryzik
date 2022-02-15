2cd342fc1a31d7c379e675bf1ea34c70f01abbdf
JavaKira <JavaKira@yandex.ru>
2022-02-15 21:13:55 +0300

    Added OilBarrel and OilBarrel destroyEffect

---
+++ Assets/Scripts/DestroyEffect.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class DestroyEffect : MonoBehaviour
+{
+    public UnityEvent started = new UnityEvent();
+    
+    public void StartEffect()
+    {
+        started.Invoke();
+    }
+}
